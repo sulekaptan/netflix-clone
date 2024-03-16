241b2f804937c41da917132f97a8239df465c0b7
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-03-16 23:14:36 +0300

    refactoring TableViewHeader Hero title

--- NetflixClone/Controllers/Core/HomeViewController.swift
+++ NetflixClone/Controllers/Core/HomeViewController.swift
@@ -18,6 +18,9 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
     let sectionTitles : [String] = ["Trending Movies", "Popular", "Trending Tv", "Upcoming Movies", "Top Rated"]
     
     private let homeFeedTable : UITableView = {
@@ -36,10 +39,24 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.rowHeight = 200
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
         configureNavbar()
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView(){
+        APICaller.shared.getTrendingMovies {[weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self?.randomTrendingMovie = titles.randomElement()
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavbar(){
