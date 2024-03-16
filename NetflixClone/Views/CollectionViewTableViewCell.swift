0068260393ff7b2b33dc8d88d40150f3a6f4e7ff
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-03-17 00:29:37 +0300

    handling tapping across all ViewControllers

--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -53,6 +53,18 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath) {
+//        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+//            switch result {
+//            case .success():
+//                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//        }
+        print("Downloading \(titles[indexPath.row].original_title)")
+    }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
@@ -92,5 +104,18 @@ extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionV
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+            
+        let config = UIContextMenuConfiguration(
+            identifier: nil,
+            previewProvider: nil) {[weak self] _ in
+                let downloadAction = UIAction(title: "Download", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
+                    self?.downloadTitleAt(indexPath: indexPath)
+                }
+                return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
+            }
+        return config
     }
 }
