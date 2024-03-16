be646743c339c0289bdc22a7fffdc617bb9fe91f
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-03-16 21:19:56 +0300

    create TitlePreviewViwController p1

---
+++ NetflixClone/Controllers/General/TitlePreviewViewController.swift
@@ -0,0 +1,73 @@
+//
+//  TitlePreviewViewController.swift
+//  NetflixClone
+//
+//  Created by Şule Kaptan on 16.03.2024.
+//
+
+import UIKit
+import WebKit
+
+class TitlePreviewViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.text = "Harry Potter"
+        return label
+    }()
+    
+    private let overviewLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        label.text = "This is the best movie ever to watch as a kid!"
+        return label
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+        button.setTitle("Download", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let webView : WKWebView = WKWebView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(webView)
+        view.addSubview(titleLabel)
+        view.addSubview(overviewLabel)
+        view.addSubview(downloadButton)
+        
+        configureConstraints()
+    }
+    
+    func configureConstraints(){
+        let webViewConstraints = [
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        NSLayoutConstraint.activate(webViewConstraints)
+        
+        let titleLabelConstraints = [
+            titleLabel.topAnchor.constraint(equalTo: webView.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ]
+        NSLayoutConstraint.activate(titleLabelConstraints)
+        
+        let overviewLabelConstraints = [
+            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
+            overviewLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ]
+        NSLayoutConstraint.activate(overviewLabelConstraints)
+    }
+    
+}
