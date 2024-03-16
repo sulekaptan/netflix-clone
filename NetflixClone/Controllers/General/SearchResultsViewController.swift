dcde16f6e5dacb00126390103ca4687d0a0e5e5a
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-03-16 17:03:42 +0300

    querying database for individual movie

--- NetflixClone/Controllers/General/SearchResultsViewController.swift
+++ NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SearchResultsViewController: UIViewController {
     
-    private var titles: [Title] = [Title]()
+    public var titles: [Title] = [Title]()
     
     public let searchResultsCollectionView: UICollectionView = {
        let layout = UICollectionViewFlowLayout()
@@ -38,14 +38,15 @@ class SearchResultsViewController: UIViewController {
 
 extension SearchResultsViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
             return UICollectionViewCell()
         }
-        cell.backgroundColor = .systemPink
+        let title = titles[indexPath.row]
+        cell.configure(with: title.poster_path ?? "")
         return cell
     }
     
